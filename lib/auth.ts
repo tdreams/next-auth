ee0005815cb583c2c024e6e0eb3f01bfca70751d
tdreams <fabriels@gmail.com>
2023-07-25 20:54:26 -0700

    auth

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Sign In",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email address",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "Admin", email: "admin@admin.com" };
+        return user;
+      },
+    }),
+  ],
+};
